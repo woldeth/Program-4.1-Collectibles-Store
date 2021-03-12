dae0084a9a7847aa750c94580956bed74305a0e7
Tomas Woldemichael <woldeth7@gmail.com>
2021-03-12 06:39:41 -0600

    DONE W/ COMMENTS

--- Transaction.h
+++ Transaction.h
@@ -1,45 +1,46 @@
 // ------------------------------------------------------------------------
 // Name: Tomas H Woldemichael
-// Date: Feb 11, 2021
-// File Name: Transaction.h
+// Date: March 11, 2021
+// File Name: Transactions.h
 // Title: PROGRAM 4
 // -------------------------------------------------------------------------
+// Transaction class:
+//   Implements an abstract class used to perform factory method design
+//   pattern excutions. This class is meant to be overridden in the subclass
+//   that will define the actual logic
+//---------------------------------------------------------------------------
 
 #pragma once
 #include <iostream>
 #include <string>
 #include "SearchTreeItems.h"
 #include "SearchTreeCustomers.h"
-//#include "HashTable.h"
+
 class HashTable;
 class SearchTreeCustomers;
 
-
 using namespace std;
 
+// Node to holds the transactions record
 struct transactionNode
 {
-    transactionNode() : item(nullptr) , transType('?'), next(nullptr) {}
-    transactionNode(Item* i, char c) : item(i) , transType(c), next(nullptr) {}
-    ~transactionNode(){}
-    Item *item;               // pointer to item that was sold
-    char transType;      // type of transaction (Buy or Sell)
+    transactionNode() : item(nullptr), transType('?'), next(nullptr) {}
+    transactionNode(Item *i, char c) : item(i), transType(c), next(nullptr) {}
+    ~transactionNode() {}
+    Item *item;            // pointer to item that was sold
+    char transType;        // type of transaction (Buy or Sell)
     transactionNode *next; // next transaction for the customer
 };
 
-//Node to hold customer obj and Head of transaction list.
+// Node to hold customer obj and Head of transaction list.
 struct customerNode
 {
     Customer *cust;        // customer ptr
-    transactionNode *head; // first transaction/ head pointer
-    customerNode() : cust(nullptr), head(nullptr){}
-    customerNode(Customer* c) : cust(c), head(nullptr) {}
-    ~customerNode(){}
-   
+    transactionNode *head; // first transaction head pointer
+    customerNode() : cust(nullptr), head(nullptr) {}
+    customerNode(Customer *c) : cust(c), head(nullptr) {}
+    ~customerNode() {}
 };
-
-
-
 
 class Transaction
 {
@@ -48,13 +49,13 @@ public:
     char command;
     int customer;
 
-    Transaction() : command('?'), customer(-1) {}
-    Transaction(char c, int cust) : command(c), customer(cust) {}
-    virtual ~Transaction() {} // deconstructor
+    Transaction() : command('?'), customer(-1) {}                 // dummy constructor
+    Transaction(char c, int cust) : command(c), customer(cust) {} // constructor with parameters
+    virtual ~Transaction() {}                                     // deconstructor
 
-    ///--------------------------------- operator== ------------------------------------
-    // creates a new item and returns it.
-    // Preconditions: None
-    // Postconditions: Create and returns a new item
+    ///--------------------------------- excute ------------------------------------
+    // Viratual abstract method used to create a new type that inherits fom this class
+    // Preconditions: Must be overriden and defined in subclass
+    // Postconditions: Creates and returns a new item for nescaru transactions
     virtual void excute(ifstream &infile, SearchTreeItems inventoryItems[], SearchTreeCustomers &stCustomers, customerNode custTransactionList[], HashTable &H) = 0;
 };
