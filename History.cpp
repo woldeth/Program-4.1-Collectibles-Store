99649d6fb97cc3429d295b896b248e4bf015e95b
Tomas Woldemichael <woldeth7@gmail.com>
2021-03-12 07:14:10 -0600

    DONE W/ COMMENTS

--- History.cpp
+++ History.cpp
@@ -1,6 +1,21 @@
+// ------------------------------------------------------------------------
+// Name: Tomas H Woldemichael
+// Date: March 11, 2021
+// File Name: History.cpp
+// Title: PROGRAM 4
+// -------------------------------------------------------------------------
+// History class:
+//   Prints alls the transactions of every customer in alpha numeric order
+//   This uses the search tree of customer to traverse in order and the id
+//   to access the transaction nodes.
+//---------------------------------------------------------------------------
 
 #include "History.h"
 
+///--------------------------------- excute ------------------------------------
+// Prints the history of every customer by customer name
+// Preconditions: None
+// Postconditions: The item will print the entire history of transaction in alpha order
 void History::excute(ifstream &infile, SearchTreeItems inventoryItems[], SearchTreeCustomers &stCustomers, customerNode custTransactionList[], HashTable &H)
 {
     stCustomers.history(custTransactionList);
--- History.h
+++ History.h
@@ -1,9 +1,14 @@
 // ------------------------------------------------------------------------
 // Name: Tomas H Woldemichael
-// Date: March 6th, 2021
+// Date: March 11, 2021
 // File Name: History.h
 // Title: PROGRAM 4
 // -------------------------------------------------------------------------
+// History class:
+//   Prints alls the transactions of every customer in alpha numeric order
+//   This uses the search tree of customer to traverse in order and the id
+//   to access the transaction nodes.
+//---------------------------------------------------------------------------
 
 #pragma once
 
@@ -13,14 +18,17 @@
 
 using namespace std;
 
-
-
 class History : public Transaction
 {
 
 public:
-    History() : Transaction('H', -1) {}
-    History(int cust) : Transaction('H' , cust) {}
-    virtual ~History() {} 
+    History() : Transaction('H', -1) {}           // dummy constructor
+    History(int cust) : Transaction('H', cust) {} // constructor w/ paramters
+    virtual ~History() {}                         // destructor
+
+    ///--------------------------------- excute ------------------------------------
+    // Prints the history of every customer by customer name
+    // Preconditions: None
+    // Postconditions: The item will print the entire history of transaction in alpha order
     virtual void excute(ifstream &infile, SearchTreeItems inventoryItems[], SearchTreeCustomers &stCustomers, customerNode custTransactionList[], HashTable &H);
 };
