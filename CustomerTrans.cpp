cbbb938a94ad7953b002ec4f62e27830df756ab9
Tomas Woldemichael <woldeth7@gmail.com>
2021-03-12 07:08:34 -0600

    COMMIT W/ COMMENTS

--- CustomerTrans.cpp
+++ CustomerTrans.cpp
@@ -1,36 +1,54 @@
 
+// ------------------------------------------------------------------------
+// Name: Tomas H Woldemichael
+// Date: March 11, 2021
+// File Name: CustomerTrans.h
+// Title: PROGRAM 4
+// -------------------------------------------------------------------------
+// CustomerTrans class:
+//   Inherits from transaction class and defines the excute method to
+//   print all the transactions for a specifc customer based on id
+//---------------------------------------------------------------------------
+
 #include "CustomerTrans.h"
 
+///--------------------------------- excute ------------------------------------
+// Prints all the customer transactions
+// Preconditions: None
+// Postconditions: Traverses the transaction nodes and prints the content recorded
 void CustomerTrans::excute(ifstream &infile, SearchTreeItems inventoryItems[], SearchTreeCustomers &stCustomers, customerNode custTransactionList[], HashTable &H)
 {
     string stringID;
     int customerID;
 
-
-    getline(infile, stringID, '\n'); //get QTY
+    getline(infile, stringID, '\n');        //get id 
     customerID = atoi(stringID.c_str());
-    
-    if(custTransactionList[customerID].head == nullptr){
 
-        if(custTransactionList[customerID].cust != nullptr){
+    if (custTransactionList[customerID].head == nullptr)
+    {
+
+        if (custTransactionList[customerID].cust != nullptr)
+        {
             custTransactionList[customerID].cust->toString();
             cout << "- no transactions" << endl;
             return;
-        }else {
-            cout << "Customer ID: " << stringID << " " << "Name: UNKNOWN" << endl;
+        }
+        else
+        {
+            cout << "Customer ID: " << stringID << " "
+                 << "Name: UNKNOWN" << endl;
             return;
         }
     }
 
-
     custTransactionList[customerID].cust->toString();
     transactionNode *cur = custTransactionList[customerID].head;
-    while(cur != nullptr){
+    while (cur != nullptr)
+    {
         cout << "transaction type: " << cur->transType << " --> ";
         cur->item->toString();
         cur = cur->next;
     }
 
     cout << endl;
-    
 }
--- CustomerTrans.h
+++ CustomerTrans.h
@@ -1,10 +1,13 @@
 // ------------------------------------------------------------------------
 // Name: Tomas H Woldemichael
-// Date: March 6th, 2021
+// Date: March 11, 2021
 // File Name: CustomerTrans.h
 // Title: PROGRAM 4
 // -------------------------------------------------------------------------
-
+// CustomerTrans class:
+//   Inherits from transaction class and defines the excute method to
+//   print all the transactions for a specifc customer based on id
+//---------------------------------------------------------------------------
 #pragma once
 
 #include <iostream>
@@ -13,14 +16,17 @@
 
 using namespace std;
 
-
 class CustomerTrans : public Transaction
 {
 
 public:
-    
-    CustomerTrans() : Transaction('C', -1) {}
-    CustomerTrans(int cust) : Transaction('C', cust) {}
-    virtual ~CustomerTrans() {} 
+    CustomerTrans() : Transaction('C', -1) {}           // dummy constructor
+    CustomerTrans(int cust) : Transaction('C', cust) {} // constructor w/ parameters
+    virtual ~CustomerTrans() {}                         // destructor
+
+    ///--------------------------------- excute ------------------------------------
+    // Prints all the customer transactions
+    // Preconditions: None
+    // Postconditions: Traverses the transaction nodes and prints the content recorded
     virtual void excute(ifstream &infile, SearchTreeItems inventoryItems[], SearchTreeCustomers &stCustomers, customerNode custTransactionList[], HashTable &H);
 };
